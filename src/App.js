539e1b68d5da87400eaaaeb189da8b494859b708
Ivan Stojanovikj <dzigi_14@hotmail.com>
2022-05-10 23:21:29 +0200

    React-commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import { useEffect, useState } from "react";
 import './App.css';
-
 function App() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const url = "https://jsonplaceholder.typicode.com/posts";
+    fetch(url)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => setPosts(data));
+  });
+
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="jumbotron">
+        <h1 className="display-4">Blog posts</h1>
+      </div>
+      {posts.map((post) => (
+        <div className="card" key={post.id}>
+          <div className="card-header">
+            #{post.id} {post.title}
+          </div>
+          <div className="card-body">
+            <p className="card-text">{post.body}</p>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
